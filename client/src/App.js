30faa7ccc930b29daf7bae53822cf4c6effd2942
mukul65 <mukul.singh@zentekinfsoft.com>
2023-10-20 10:37:47 +0530

    data

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { Container, Grid, Grow } from '@mui/material';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Auth from './components/Auth/Auth';
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Container maxWidth="lg">
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/auth' element={<Auth />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { reducers } from './reducers';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
